e98450850cd8656a5e4921012cb0d1dbd994c6b4
Jeremy <jeremylshannon@gmail.com>
2022-05-18 14:36:28 -0700

    Dead fires can be relit by neighbors, with new inferno.ignite() function

--- mods/tech/fires.lua
+++ mods/tech/fires.lua
@@ -26,6 +26,8 @@ local char_temp_effect = wood_temp_effect * 2
 local char_temp_max = wood_temp_max * 2
 local char_air_c = 0.45
 
+inferno = inferno
+
 -------------------------------
 --Functions
 
@@ -743,7 +745,9 @@ minetest.register_node('tech:small_wood_fire_ext', {
 
 	on_dig = on_dig_fire,
 	after_place_node = after_place_fire,
-
+	on_burn = function(pos)
+	   inferno.ignite(pos)
+	end,
 })
 
 
@@ -755,6 +759,9 @@ minetest.register_node('tech:large_wood_fire_ext', {
 
 	on_dig = on_dig_fire,
 	after_place_node = after_place_fire,
+	on_burn = function(pos)
+	   inferno.ignite(pos)
+	end,
 
 })
 
@@ -775,6 +782,9 @@ minetest.register_node('tech:small_charcoal_fire_ext', {
 
 	on_dig = on_dig_fire,
 	after_place_node = after_place_fire,
+	on_burn = function(pos)
+	   inferno.ignite(pos)
+	end,
 
 })
 
@@ -787,6 +797,9 @@ minetest.register_node('tech:large_charcoal_fire_ext', {
 
 	on_dig = on_dig_fire,
 	after_place_node = after_place_fire,
+	on_burn = function(pos)
+	   inferno.ignite(pos)
+	end,
 
 })
 
